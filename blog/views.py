1dd0dfb051a388c27957f2b13683b8d04b266c3a
BTemplar <97474689+BTemplar@users.noreply.github.com>
2025-02-20 01:51:05 +0400

    Added handling of PageNotAnInteger and EmptyPage errors when the page request is incorrect.

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
     try:
         post_list = Post.published.all()
         paginator = Paginator(post_list, 3)
         page_number = request.GET.get('page', 1)
-        posts = paginator.page(page_number)
+        try:
+            posts = paginator.page(page_number)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
     except Post.DoesNotExist:
         raise Http404("No posts published yet.")
     return render(request, 'post/list.html', {'posts': posts})
